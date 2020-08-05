2d63b02cc2e47fc8c716dccd4335f8ee2f72900d
hsk9044 <qyr9044@hotmail.com>
2020-08-05 10:24:57 +0800

    修改title

--- views/layouts/admin.blade.php
+++ views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')</title>
+    <title>{{config('admin.system_name')}}-@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('vendor/laravel-layui-admin/lib/layui/css/layui.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/laravel-layui-admin/css/admin.css') }}">
 </head>
